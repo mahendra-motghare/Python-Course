5bae2e867e8efe9e4ec702b4596b53a1117b8d6b
Mahendra Motghare <115101870+mahendra-motghare@users.noreply.github.com>
2026-03-06 22:10:16 +0530

    Delete Simple interest.py

--- Simple interest.py
+++
@@ -1,5 +0,0 @@
-# Calculate simple interest
-P = float(input("Enter Principal amount: "))
-R = float(input("Enter Rate of interest per annum (in %): "))
-T = int(input("Enter Time period (in years): "))
-print("Simple Interest =", (P*R*T)/100)
