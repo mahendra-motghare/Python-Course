0e17a06318920d4f0256c0c73c7370becb0fee28
Mahendra Motghare <115101870+mahendra-motghare@users.noreply.github.com>
2026-03-06 22:08:42 +0530

    Add files via upload

---
+++ Lab 3/Simple interest.py
@@ -0,0 +1,5 @@
+# Calculate simple interest
+P = float(input("Enter Principal amount: "))
+R = float(input("Enter Rate of interest per annum (in %): "))
+T = int(input("Enter Time period (in years): "))
+print("Simple Interest =", (P*R*T)/100)
---
+++ Lab 3/Sum of two nums.py
@@ -0,0 +1,4 @@
+# Sum of two numbers
+n1 = float(input("Enter n1: "))
+n2 = float(input("Enter n2: "))
+print("Sum = ", n1+n2)
